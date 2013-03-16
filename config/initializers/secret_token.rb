92c0e79e9422bc9960dd4c463befad42ea6fa06b
ryonext <ryonext.s@gmail.com>
2013-03-16 23:19:13 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toko::Application.config.secret_key_base = 'c6ad33d28605dc6da8c41bb6d5447ad956e9ffa7d5350ec4ef5b9677a2c6a495191afd5c518c2722d9dc5d9f29e94af1281821a8973305534bf9f7fd1ab37f9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Toko::Application.config.session_store :encrypted_cookie_store, key: '_Toko_session'

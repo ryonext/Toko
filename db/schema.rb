a1d6afc65cd0ffa982c8d94abd2021b9f3525e5b
ryonext <ryo.next@gmail.com>
2013-03-31 13:51:03 +0900

    done 'create owner workspace

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130331024359) do
-
-  create_table "auths", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",     limit: 128, default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "auths", ["email"], name: "index_auths_on_email", unique: true
-  add_index "auths", ["reset_password_token"], name: "index_auths_on_reset_password_token", unique: true
+ActiveRecord::Schema.define(version: 20130322145243) do
 
   create_table "users", force: true do |t|
-    t.string   "username",         null: false
-    t.string   "email"
-    t.string   "crypted_password"
-    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
+    t.string   "name"
   end
 
   create_table "workspaces", force: true do |t|

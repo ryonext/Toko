5c196eee0a670462192965864e02e9ac42a224be
ryonext <ryonext.s@gmail.com>
2013-04-01 00:01:33 +0900

    show only my workspace

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -31,7 +31,7 @@ class WorkspacesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_workspace
-      @workspace = Workspace.find(params[:id])
+      @workspace = Workspace.find_by(:id => params[:id], :owner_id => current_user.id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
